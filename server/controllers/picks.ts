7c59bf6a7cdc2c29612f2a039de2dc80f9c327ac
Richard Dubay <richardtdubayjr@gmail.com>
2024-08-02 14:23:58 -0400

    Updated the picks model/controller to use try/catch blocks

--- server/controllers/picks.ts
+++ server/controllers/picks.ts
@@ -3,109 +3,123 @@ import { picksModel } from "../models";
 import { prisma } from "../utils/prisma";
 
 const deletePick = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const numericId = parseInt(id);
-  const pick = await prisma.picks.findUnique({
-    where: {
-      id: numericId,
-    },
-  });
-  if (pick) {
-    const deletedPick = await picksModel.deletePick(numericId);
-    res.json({
-      message: "Deleted pick",
-      pick: {
-        id: deletedPick.id,
-        player_id: deletedPick.player_id,
-        grand_prix_id: deletedPick.grand_prix_id,
-        choice_id: deletedPick.choice_id,
-        pick_id: deletedPick.pick_id,
-        created_at: deletedPick.created_at,
+  try {
+    const { id } = req.params;
+    const numericId = parseInt(id);
+    const pick = await prisma.picks.findUnique({
+      where: {
+        id: numericId,
       },
     });
-  } else {
-    res.send("No pick with that id exists");
+    if (pick) {
+      const deletedPick = await picksModel.deletePick(numericId);
+      res.json({
+        message: "Deleted pick",
+        pick: {
+          id: deletedPick.id,
+          player_id: deletedPick.player_id,
+          grand_prix_id: deletedPick.grand_prix_id,
+          choice_id: deletedPick.choice_id,
+          pick_id: deletedPick.pick_id,
+          created_at: deletedPick.created_at,
+        },
+      });
+    } else {
+      res.send("No pick with that id exists");
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error,
+    });
   }
 };
 
 const getPicks = async (req: Request, res: Response) => {
-  const picks = await picksModel.getAllPicks();
-  res.json(picks);
+  try {
+    const picks = await picksModel.getAllPicks();
+    res.json(picks);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error,
+    });
+  }
 };
 
 const getPicksByGrandPrixId = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const numericId = parseInt(id);
-  const pick = await picksModel.getPicksByGrandPrixId(numericId);
-  res.json(pick);
+  try {
+    const { id } = req.params;
+    const numericId = parseInt(id);
+    const pick = await picksModel.getPicksByGrandPrixId(numericId);
+    res.json(pick);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error,
+    });
+  }
 };
 
 const postPick = async (req: Request, res: Response) => {
-  const { player_id, grand_prix_id, choice_id, pick_id } = req.body;
-  const newPick = {
-    player_id,
-    grand_prix_id,
-    choice_id,
-    pick_id,
-    created_at: new Date(),
-  };
+  try {
+    const { player_id, grand_prix_id, choice_id, pick_id } = req.body;
+    const newPick = {
+      player_id,
+      grand_prix_id,
+      choice_id,
+      pick_id,
+      created_at: new Date(),
+    };
 
-  if (
-    !newPick.player_id ||
-    !newPick.grand_prix_id ||
-    !newPick.choice_id ||
-    !newPick.pick_id
-  ) {
-    return res.status(400).send("Missing items needed to create a pick");
-  }
+    if (
+      !newPick.player_id ||
+      !newPick.grand_prix_id ||
+      !newPick.choice_id ||
+      !newPick.pick_id
+    ) {
+      return res.status(400).send("Missing items needed to create a pick");
+    }
 
-  const pickResponse = await picksModel.postPick(newPick);
+    const pickResponse = await picksModel.postPick(newPick);
 
-  if ("errorMessage" in pickResponse) {
-    console.error(
-      `There was an error adding the new pick: ${pickResponse.errorMessage}`
-    );
-    return res
-      .status(pickResponse.statusCode)
-      .send(
-        `There was an error adding the new pick: ${pickResponse.errorMessage}`
-      );
-  } else {
-    return res.status(pickResponse.statusCode).json({
+    return res.status(201).json({
       message: "Pick created successfully",
-      pick: pickResponse.pick,
+      pick: pickResponse,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `There was an error adding the new pick: ${error}`,
+      error: error,
     });
   }
 };
 
 const putPick = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const numericId = parseInt(id);
-  const { player_id, grand_prix_id, choice_id, pick_id, created_at } = req.body;
+  try {
+    const { id } = req.params;
+    const numericId = parseInt(id);
+    const { player_id, grand_prix_id, choice_id, pick_id, created_at } =
+      req.body;
 
-  const pickToUpdate = {
-    player_id,
-    grand_prix_id,
-    choice_id,
-    pick_id,
-    created_at,
-  };
+    const pickToUpdate = {
+      player_id,
+      grand_prix_id,
+      choice_id,
+      pick_id,
+      created_at,
+    };
 
-  const pickResponse = await picksModel.putPick(numericId, pickToUpdate);
+    const pickResponse = await picksModel.putPick(numericId, pickToUpdate);
 
-  if ("errorMessage" in pickResponse) {
-    console.error(
-      `There was an error updating the pick: ${pickResponse.errorMessage}`
-    );
-    return res
-      .status(pickResponse.statusCode)
-      .send(
-        `There was an error updating the pick: ${pickResponse.errorMessage}`
-      );
-  } else {
-    return res.status(pickResponse.statusCode).json({
+    return res.status(200).json({
       message: "Pick updated successfully",
-      pick: pickResponse.pick,
+      pick: pickResponse,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `There was an error updating the pick: ${error}`,
+      error: error,
     });
   }
 };
--- server/models/picks.ts
+++ server/models/picks.ts
@@ -31,51 +31,26 @@ export const getPicksByGrandPrixId = async (grandPrixId: number) => {
 };
 
 export const postPick = async (pick: PickType) => {
-  try {
-    const newPick = await prisma.picks.create({
-      data: {
-        player_id: pick.player_id,
-        grand_prix_id: pick.grand_prix_id,
-        choice_id: pick.choice_id,
-        pick_id: pick.pick_id,
-        created_at: pick.created_at,
-      },
-    });
-    return {
-      message: "Success",
-      statusCode: 201,
-      pick: newPick,
-    };
-  } catch (error) {
-    return {
-      message: "Internal Server Error",
-      statusCode: 500,
-      errorMessage: error,
-    };
-  }
+  const newPick = await prisma.picks.create({
+    data: {
+      player_id: pick.player_id,
+      grand_prix_id: pick.grand_prix_id,
+      choice_id: pick.choice_id,
+      pick_id: pick.pick_id,
+      created_at: pick.created_at,
+    },
+  });
+  return newPick;
 };
 
 export const putPick = async (pickId: number, pick: PickType) => {
-  try {
-    const updatedPick = await prisma.picks.update({
-      where: {
-        id: pickId,
-      },
-      data: {
-        ...pick,
-      },
-    });
-
-    return {
-      message: "Success",
-      statusCode: 200,
-      pick: updatedPick,
-    };
-  } catch (error) {
-    return {
-      message: "Internal Server Error",
-      statusCode: 500,
-      errorMessage: error,
-    };
-  }
+  const updatedPick = await prisma.picks.update({
+    where: {
+      id: pickId,
+    },
+    data: {
+      ...pick,
+    },
+  });
+  return updatedPick;
 };
