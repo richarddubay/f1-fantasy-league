874a60dc6c331d2869386a0f3a8f716226605208
Richard Dubay <richardtdubayjr@gmail.com>
2024-08-02 14:27:12 -0400

    Updated the players model/controller to use try/catch blocks

--- server/controllers/players.ts
+++ server/controllers/players.ts
@@ -3,104 +3,117 @@ import { playersModel } from "../models";
 import { prisma } from "../utils/prisma";
 
 const deletePlayer = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const numericId = parseInt(id);
-  const player = await prisma.player.findUnique({
-    where: {
-      id: numericId,
-    },
-  });
-  if (player) {
-    const deletedPlayer = await playersModel.deletePlayer(numericId);
-    res.send(
-      `Deleted player ${deletedPlayer.id}: ${deletedPlayer.first_name} ${deletedPlayer.last_name}`
-    );
-    res.json({
-      message: "Deleted player",
-      pick: {
-        id: deletedPlayer.id,
-        first_name: deletedPlayer.first_name,
-        last_name: deletedPlayer.last_name,
-        created_at: deletedPlayer.created_at,
+  try {
+    const { id } = req.params;
+    const numericId = parseInt(id);
+    const player = await prisma.player.findUnique({
+      where: {
+        id: numericId,
       },
     });
-  } else {
-    res.send("No player with that id exists");
+    if (player) {
+      const deletedPlayer = await playersModel.deletePlayer(numericId);
+      res.send(
+        `Deleted player ${deletedPlayer.id}: ${deletedPlayer.first_name} ${deletedPlayer.last_name}`
+      );
+      res.json({
+        message: "Deleted player",
+        pick: {
+          id: deletedPlayer.id,
+          first_name: deletedPlayer.first_name,
+          last_name: deletedPlayer.last_name,
+          created_at: deletedPlayer.created_at,
+        },
+      });
+    } else {
+      res.send("No player with that id exists");
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error,
+    });
   }
 };
 
 const getPlayers = async (req: Request, res: Response) => {
-  const players = await playersModel.getAllPlayers();
-  res.json(players);
+  try {
+    const players = await playersModel.getAllPlayers();
+    res.json(players);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error,
+    });
+  }
 };
 
 const getPlayerById = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const numericId = parseInt(id);
-  const player = await playersModel.getPlayerById(numericId);
-  res.json(player);
+  try {
+    const { id } = req.params;
+    const numericId = parseInt(id);
+    const player = await playersModel.getPlayerById(numericId);
+    res.json(player);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error,
+    });
+  }
 };
 
 const postPlayer = async (req: Request, res: Response) => {
-  const { first_name, last_name } = req.body;
-  const newPlayer = {
-    first_name,
-    last_name,
-    created_at: new Date(),
-  };
+  try {
+    const { first_name, last_name } = req.body;
+    const newPlayer = {
+      first_name,
+      last_name,
+      created_at: new Date(),
+    };
 
-  if (!newPlayer.first_name || !newPlayer.last_name) {
-    return res.status(400).send("A player needs a first and last name.");
-  }
+    if (!newPlayer.first_name || !newPlayer.last_name) {
+      return res.status(400).send("A player needs a first and last name.");
+    }
 
-  const playerResponse = await playersModel.postPlayer(newPlayer);
+    const playerResponse = await playersModel.postPlayer(newPlayer);
 
-  if ("errorMessage" in playerResponse) {
-    console.error(
-      `There was an error adding the new player: ${playerResponse.errorMessage}`
-    );
-    return res
-      .status(playerResponse.statusCode)
-      .send(
-        `There was an error adding the new player: ${playerResponse.errorMessage}`
-      );
-  } else {
-    return res.status(playerResponse.statusCode).json({
+    return res.status(201).json({
       message: "Player created successfully",
-      player: playerResponse.player,
+      player: playerResponse,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `There was an error adding the new player: ${error}`,
+      error: error,
     });
   }
 };
 
 const putPlayer = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const numericId = parseInt(id);
-  const { first_name, last_name, created_at } = req.body;
+  try {
+    const { id } = req.params;
+    const numericId = parseInt(id);
+    const { first_name, last_name, created_at } = req.body;
 
-  const playerToUpdate = {
-    first_name,
-    last_name,
-    created_at,
-  };
+    const playerToUpdate = {
+      first_name,
+      last_name,
+      created_at,
+    };
 
-  const playerResponse = await playersModel.putPlayer(
-    numericId,
-    playerToUpdate
-  );
-
-  if ("errorMessage" in playerResponse) {
-    console.error(
-      `There was an error updating the player: ${playerResponse.errorMessage}`
+    const playerResponse = await playersModel.putPlayer(
+      numericId,
+      playerToUpdate
     );
-    return res
-      .status(playerResponse.statusCode)
-      .send(
-        `There was an error updating the player: ${playerResponse.errorMessage}`
-      );
-  } else {
-    return res.status(playerResponse.statusCode).json({
+
+    return res.status(200).json({
       message: "Player updated successfully",
-      player: playerResponse.player,
+      player: playerResponse,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `There was an error updating the player: ${error}`,
+      error: error,
     });
   }
 };
--- server/models/players.ts
+++ server/models/players.ts
@@ -31,49 +31,24 @@ export const getPlayerById = async (playerId: number) => {
 };
 
 export const postPlayer = async (player: PlayerType) => {
-  try {
-    const newPlayer = await prisma.player.create({
-      data: {
-        first_name: player.first_name,
-        last_name: player.last_name,
-        created_at: player.created_at,
-      },
-    });
-    return {
-      message: "Success",
-      statusCode: 201,
-      player: newPlayer,
-    };
-  } catch (error) {
-    return {
-      message: "Internal Server Error",
-      statusCode: 500,
-      errorMessage: error,
-    };
-  }
+  const newPlayer = await prisma.player.create({
+    data: {
+      first_name: player.first_name,
+      last_name: player.last_name,
+      created_at: player.created_at,
+    },
+  });
+  return newPlayer;
 };
 
 export const putPlayer = async (playerId: number, player: PlayerType) => {
-  try {
-    const updatedPlayer = await prisma.player.update({
-      where: {
-        id: playerId,
-      },
-      data: {
-        ...player,
-      },
-    });
-
-    return {
-      message: "Success",
-      statusCode: 200,
-      player: updatedPlayer,
-    };
-  } catch (error) {
-    return {
-      message: "Internal Server Error",
-      statusCode: 500,
-      errorMessage: error,
-    };
-  }
+  const updatedPlayer = await prisma.player.update({
+    where: {
+      id: playerId,
+    },
+    data: {
+      ...player,
+    },
+  });
+  return updatedPlayer;
 };
